73d0d510d77e24375e62cf50db876a630b132190
Brightello <brightellojj@gmail.com>
2025-03-15 11:12:05 +0100

    Basic project configuration

---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Box} from "@chakra-ui/react";
+
+function Dashboard() {
+    return (
+        <Box w="100%">
+
+
+        </Box>
+    );
+}
+
+export default Dashboard;
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import Dashboard from "./Dashboard.jsx";
+import Register from "./Register.jsx";
+import Login from "./Login.jsx";
+import PrivateRoute from "./PrivateRoute.jsx";
+
+export {
+    Dashboard,
+    Register,
+    Login,
+    PrivateRoute
+
+}
