d99a70dcc0b5e42757627d3f995757ab993e9c2a
Brait <brightellojj@gmail.com>
2025-05-07 16:31:35 +0200

    fix: change the look of the post modal

--- src/components/CustomModal.jsx
+++ src/components/CustomModal.jsx
@@ -9,7 +9,7 @@ function CustomModal({ children, isOpen, onClose }) {
         maxWidth={{ base: "90%", sm: "80%", md: "70%" }}
         height={{ base: "auto", md: "650px" }}
       >
-        <ModalBody maxW="100%" p="0" h="100%">
+        <ModalBody p="0" h="100%">
           {children}
         </ModalBody>
       </ModalContent>
